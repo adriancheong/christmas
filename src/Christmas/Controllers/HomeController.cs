e3bfcb3c11d49843dd8dcfb6b516b5cc4054cae7
Adrian Cheong <adriancheong@gic.com.sg>
2016-11-20 15:12:32 +0800

    Redirect Home View to gamepage

--- src/Christmas/Controllers/HomeController.cs
+++ src/Christmas/Controllers/HomeController.cs
@@ -11,21 +11,8 @@ namespace Christmas.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return RedirectToAction("Game");
+            //return View();
         }
 
         public IActionResult Game()
